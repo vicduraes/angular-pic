e1c79a4a537ac6f33990d70c6e05f8fd69b923e1
vicduraes <duraes.vic@gmail.com>
2020-09-30 17:07:32 -0300

    feat: create photo module and call API to view photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { PhotosModule } from './photos/photos.module';
+import { AppComponent } from "./app.component";
+import { PhotosModule } from "./photos/photos.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
