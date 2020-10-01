9fe025fea49ca2fdd3eea050ad7e325d444e370a
vicduraes <duraes.vic@gmail.com>
2020-10-01 12:31:20 -0300

    feat: add filter

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,6 +6,7 @@ import { PhotoComponent } from "./photo/photo.component";
 import { PhotosListComponent } from "./photos-list/photos-list.component";
 import { PhotosFormComponent } from "./photos-form/photos-form.component";
 import { PhotosComponent } from "./photos-list/photos/photos.component";
+import { FilterByDescription } from "./photos-list/filter-by-description.pipe";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PhotosComponent } from "./photos-list/photos/photos.component";
     PhotosListComponent,
     PhotosFormComponent,
     PhotosComponent,
+    FilterByDescription,
   ],
   imports: [HttpClientModule, CommonModule],
 })
