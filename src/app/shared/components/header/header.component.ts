abf2a49c3f3dbd5a1236194918198d8a042ae577
vicduraes <duraes.vic@gmail.com>
2020-10-02 17:38:36 -0300

    refactor: add login and logout link

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,25 @@
+import { Observable } from "rxjs";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { UserService } from "./../../../core/user.service";
+import { User } from "src/app/core/user";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  user$: Observable<User>;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.user$ = userService.getUser();
+  }
 
   ngOnInit() {}
+
+  logout() {
+    this.userService.logout();
+    this.router.navigateByUrl("");
+  }
 }
