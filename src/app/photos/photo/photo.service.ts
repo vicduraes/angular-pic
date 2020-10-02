41932525e10be11ec6a3c984d18bad0cc472419d
vicduraes <duraes.vic@gmail.com>
2020-10-02 12:19:03 -0300

    chore: create API environment variable

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Photo } from "./photo";
 
-const API = "http://localhost:3000";
+import { Photo } from "./photo";
+import { environment } from "./../../../environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class PhotoService {
   constructor(private http: HttpClient) {}
 
   listFromUser(userName: string) {
-    return this.http.get<Photo[]>(`${API}/${userName}/photos`);
+    return this.http.get<Photo[]>(`${environment.API}/${userName}/photos`);
   }
 
   listFromUserPaginated(userName: string, page: number) {
     const params = new HttpParams().append("page", page.toString());
-    return this.http.get<Photo[]>(`${API}/${userName}/photos`, { params });
+    return this.http.get<Photo[]>(`${environment.API}/${userName}/photos`, {
+      params,
+    });
   }
 }
