7a22381693c065c1810a9691dd3976bff17b58b7
vicduraes <duraes.vic@gmail.com>
2020-10-05 10:37:16 -0300

    chore: create home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SigninComponent } from "./signin/signin.component";
 import { SmallMessageModule } from "../shared/components/small-message/small-message.module";
 import { SignupComponent } from "./signup/signup.component";
+import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { SignupComponent } from "./signup/signup.component";
     SmallMessageModule,
     RouterModule,
   ],
-  declarations: [SigninComponent, SignupComponent],
+  declarations: [SigninComponent, SignupComponent, HomeComponent],
 })
 export class HomeModule {}
