b8151dee6994882cc648d959e672f8cb4bf8e534
vicduraes <duraes.vic@gmail.com>
2020-10-06 20:03:58 -0300

    style: change photo comments component

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -9,6 +9,7 @@ import { PhotoComment } from "../../photo/photo-comment";
 @Component({
   selector: "app-photo-comments",
   templateUrl: "./photo-comments.component.html",
+  styleUrls: ["photo-comments.component.css"],
 })
 export class PhotoCommentsComponent implements OnInit {
   @Input() photoId: number;
