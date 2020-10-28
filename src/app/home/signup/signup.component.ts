c711d1104d53836651cebe218552179a5dc2a069
vicduraes <duraes.vic@gmail.com>
2020-10-27 22:15:44 -0300

    chore: implements differents validations in signup form

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { Router } from "@angular/router";
 import { SignupService } from "./signup.service";
 import { CheckUserService } from "./check-user.service";
 import { NewUser } from "./new-user";
+import { userNamePassword } from "./username-password.validator";
 
 @Component({
   selector: "app-signup",
@@ -23,43 +24,50 @@ export class SignupComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.signupForm = this.formBuilder.group({
-      email: ["", [Validators.required, Validators.email]],
-      fullName: [
-        "",
-        [
-          Validators.required,
-          Validators.minLength(10),
-          Validators.maxLength(40),
+    this.signupForm = this.formBuilder.group(
+      {
+        email: ["", [Validators.required, Validators.email]],
+        fullName: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(40),
+          ],
         ],
-      ],
-      userName: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(/^[a-z0-9_\-]+$/),
-          Validators.minLength(4),
-          Validators.maxLength(15),
+        userName: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(/^[a-z0-9_\-]+$/),
+            Validators.minLength(4),
+            Validators.maxLength(15),
+          ],
+          this.checkUser.checkUserName(),
         ],
-        this.checkUser.checkUserName(),
-      ],
-      password: [
-        "",
-        [
-          Validators.required,
-          Validators.minLength(5),
-          Validators.maxLength(10),
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(10),
+          ],
         ],
-      ],
-    });
+      },
+      {
+        validator: userNamePassword,
+      }
+    );
     this.inputEmail.nativeElement.focus();
   }
 
   signup() {
-    const newUser = this.signupForm.getRawValue() as NewUser;
-    this.signupService.signup(newUser).subscribe(
-      () => this.router.navigate([""]),
-      (err) => console.error(err.message)
-    );
+    if (this.signupForm.valid && !this.signupForm.pending) {
+      const newUser = this.signupForm.getRawValue() as NewUser;
+      this.signupService.signup(newUser).subscribe(
+        () => this.router.navigate([""]),
+        (err) => console.error(err.message)
+      );
+    }
   }
 }
