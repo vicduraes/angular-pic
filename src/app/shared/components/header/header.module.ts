18bf773e0eb6c98d4398395eaeeea1897dc55013
vicduraes <duraes.vic@gmail.com>
2020-10-28 20:24:38 -0300

    chore: adjustment in header component

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,13 +1,20 @@
+import { OnlyLoggedModule } from "./../../directives/only-logged/only-logged.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 
 import { HeaderComponent } from "./header.component";
 import { NotificationsModule } from "../notifications/notifications.module";
-import { LoadingModule } from './../loading/loading.module';
+import { LoadingModule } from "./../loading/loading.module";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NotificationsModule, LoadingModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NotificationsModule,
+    LoadingModule,
+    OnlyLoggedModule,
+  ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
